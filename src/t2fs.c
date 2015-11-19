f51a86652a5151f7d371c5e7ba35af44a9880614
Khin Baptista <kerbaptista@inf.ufrgs.br>
2015-11-19 19:17:43 -0200

    Starting development on separate branch

--- src/t2fs.c
+++ src/t2fs.c
@@ -21,11 +21,22 @@
 #include "t2fs.h"
 
 struct t2fs_superbloco superblock;
+int initialized = 0;
+
+int identify2(char* name, int size){
+	char id[] = "JoaoLauro_KhinBaptista";
+
+	size = 22;
+	memcpy(name, id, size);
+	puts(name);
+	
+	return 0;
+}
 
 void t2fs_readSuperBlock(){
 	BYTE buffer[SECTOR_SIZE];
 	read_sector(0, (char *)buffer);
-	
+
 	memcpy(superblock.Id, buffer, 4);
 	printf(superblock.Id);
 }
