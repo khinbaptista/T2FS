1c06545410e45da5143a6020c705edc9b5c8af07
Khin Baptista <kerbaptista@inf.ufrgs.br>
2015-11-19 19:13:11 -0200

    Added identification

--- src/t2fs.c
+++ src/t2fs.c
@@ -16,22 +16,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <bool.h>
 
 #include "apidisk.h"
 #include "t2fs.h"
 
 struct t2fs_superbloco superblock;
+int initialized = 0;
+
+int identify2(char* name, int size){
+	char id[] = "JoaoLauro_KhinBaptista";
+
+	size = 22;
+	memcpy(name, id, size);
+	puts(name);
+	return 0;
+}
 
 void t2fs_readSuperBlock(){
 	BYTE buffer[SECTOR_SIZE];
 	read_sector(0, (char *)buffer);
-	
+
 	memcpy(superblock.Id, buffer, 4);
 	printf(superblock.Id);
 }
 
 void t2fs_init(){
-	t2fs_readSuperBlock();
+	if (initialized == 0){
+		t2fs_readSuperBlock();
+		initialized = 1;
+	}
 }
 
 int delete2 (char *filename){
