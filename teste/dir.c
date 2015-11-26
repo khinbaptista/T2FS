79b48686bdb00d4b351d86bcf4f2043cbf95f2d7
Khin Baptista <kerbaptista@inf.ufrgs.br>
2015-11-26 21:28:32 -0200

    Changed test

--- teste/dir.c
+++ teste/dir.c
@@ -17,7 +17,13 @@ int main(int argc, char* argv[]){
 	while (readdir2(dir, &dentry) == 0)
 		printf("%d: %s\n", dentry.fileType, dentry.name);
 
-	printf("\nClosing dir: %d\n", closedir2(dir));
-
+	printf("\nClosing dir: %d\n", dir);
+	closedir2(dir);
+	
+	chdir2("/subdir/");
+	
+	getcwd2(ids, 40);
+	printf("Current dir: %s\n", ids);
+	
 	return 0;
 }
